6e3a7e59be5774edf1550996d95de22f22e3ad00
kaomianbao <47048893+kaomianbao@users.noreply.github.com>
2023-12-11 17:29:53 +0800

    Update Handles.sh

--- Scripts/Handles.sh
+++ Scripts/Handles.sh
@@ -1,58 +1,5 @@
 #!/bin/bash
 
-#修改Tiny Filemanager汉化
-if [ -d *"tinyfilemanager"* ]; then
-	PO_FILE="./luci-app-tinyfilemanager/po/zh_Hans/tinyfilemanager.po"
-	sed -i '/msgid "Tiny File Manager"/{n; s/msgstr.*/msgstr "文件管理器"/}' $PO_FILE
-	sed -i 's/启用用户验证/用户验证/g;s/家目录/初始目录/g;s/Favicon 路径/收藏夹图标路径/g' $PO_FILE
-
-	echo "tinyfilemanager date has been updated!"
-fi
-
-#预置HomeProxy数据
-if [ -d *"homeproxy"* ]; then
-	HP_PATCH="../homeproxy/root/etc/homeproxy/resources"
-
-	UPDATE_RESOURCES() {
-		local res_type=$1
-		local res_repo=$2
-		local res_branch=$3
-		local res_file=$4
-		local res_depth=${5:-1}
-		local res_ext=${res_file##*.}
-
-		git clone -q --depth=$res_depth --single-branch --branch $res_branch "https://github.com/$res_repo.git" ./$res_type/
-
-		cd ./$res_type/
-
-		if [[ $res_ext == "txt" ]]; then
-			echo $(git log -1 --pretty=format:'%s' -- $res_file | grep -o "[0-9]*") > "$res_type".ver
-			mv -f $res_file "$res_type"."$res_ext"
-		elif [[ $res_ext == "zip" ]]; then
-			echo $(git log -1 --pretty=format:'%s' | cut -d ' ' -f 1) > "$res_type".ver
-			curl -sfL -O "https://github.com/$res_repo/archive/$res_file"
-			mv -f $res_file $HP_PATCH/"${res_repo//\//_}"."$res_ext"
-		elif [[ $res_ext == "db" ]]; then
-			local res_ver=$(git tag | tail -n 1)
-			echo $res_ver > "$res_type".ver
-			curl -sfL -O "https://github.com/$res_repo/releases/download/$res_ver/$res_file"
-		fi
-
-		cp -f "$res_type".* $HP_PATCH/
-
-		cd .. && rm -rf ./$res_type/
-	}
-
-	UPDATE_RESOURCES "china_ip4" "1715173329/IPCIDR-CHINA" "master" "ipv4.txt" "5"
-	UPDATE_RESOURCES "china_ip6" "1715173329/IPCIDR-CHINA" "master" "ipv6.txt" "5"
-	UPDATE_RESOURCES "gfw_list" "Loyalsoldier/v2ray-rules-dat" "release" "gfw.txt"
-	UPDATE_RESOURCES "china_list" "Loyalsoldier/v2ray-rules-dat" "release" "direct-list.txt"
-	UPDATE_RESOURCES "geoip" "1715173329/sing-geoip" "master" "geoip.db"
-	UPDATE_RESOURCES "geosite" "1715173329/sing-geosite" "master" "geosite.db"
-	UPDATE_RESOURCES "clash_dashboard" "MetaCubeX/metacubexd" "gh-pages" "gh-pages.zip"
-
-	echo "homeproxy date has been updated!"
-fi
 
 #预置OpenClash内核和数据
 if [ -d *"OpenClash"* ]; then
